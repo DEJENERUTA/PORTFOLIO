66bbd6f218556011609eac8a51faac7f26552ed0
DEJENERUTA <dejenedaba269@gmail.com>
2020-05-02 23:03:42 +0200

    added website after changing some stuff

---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+Console.log("hello");
+
+const button = document.querySelector(".btn");
+
+
+button.addEventListener("send", (event) => {
+    alert("Thank you for visiting my website, how can I help you??");
+})
